60c6ab4529829e7a374a0b7b3fd65aa1fbbbb131
Alex (Oleksii) Haponiuk <git@alexh.dev>
2026-05-25 15:53:09 +0100

    add playwright support

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import StartGame from './game/main';
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    StartGame('game-container');
+    const game = StartGame('game-container');
+
+    if (import.meta.env.DEV) {
+        (window as unknown as { __game__: typeof game }).__game__ = game;
+    }
 
 });
